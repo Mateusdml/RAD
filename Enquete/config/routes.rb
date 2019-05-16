a7dba9899d190b9cb260264b0f5e750403465672
Mateus de Morais <mateusdmoraisl@gmail.com>
2019-05-16 08:49:44 -0300

    Enquete V0.2

--- Enquete/config/routes.rb
+++ Enquete/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :answers
+  resources :surveys
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
